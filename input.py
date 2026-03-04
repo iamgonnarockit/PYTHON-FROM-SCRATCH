d0e1fe5d3c30088215847b5875c409b4d3e8b7b5
iamgonnarockit <adhikarisadikshya097@gmail.com>
2026-03-04 21:40:10 +0545

    Take inputs from the user

--- input.py
+++ input.py
@@ -3,11 +3,19 @@ a=input("Enter your name:") #result for this is always string
 b=int(input("Enter your age:")) #to take input values
 print("Hi there! Welcome",a,".Ohh! so you're",b,"years old")
 
-#Let's practice some questions. WAP to input 2 numbers and print their sum.
-x=int(input("Enter a number:"))
-y=int(input("Another number:"))
-print("Sum is:",x+y)
+#Let's practice some questions. WAP to input 3 numbers and print their sum.
+x=float(input("Enter a number:"))
+y=float(input("Another number:"))
+z=float(input("third number:"))
+print("Sum is:",x+y+z)
+print("Average:",(x+y+z)/3)
 
 #WAP to input side of a square and print its area.
 l=float(input("Enter the side length of a sq."))
 print("Area is:",l*l) #or it can be written as,"l**2", which means l to the power 2.
+
+
+#WAP to input 2 numbers and print true if a is greater than or equal to b. if not false. 
+first=float(input("Enter first number:"))
+second=float(input("Second number:"))
+print(first>=second)
