176694e4921e937ceba8ec57460713239e8729a0
iamgonnarockit <adhikarisadikshya097@gmail.com>
2026-03-04 21:19:36 +0545

    Take input from the user

--- input.py
+++ input.py
@@ -1,4 +1,13 @@
 #Let's learn to take inputs from the user.
 a=input("Enter your name:") #result for this is always string
-b=input(int("Enter your age:")) #to take input values
-print("Hi there! Welcome",a,"Ohh! so you're",b,"years old")
+b=int(input("Enter your age:")) #to take input values
+print("Hi there! Welcome",a,".Ohh! so you're",b,"years old")
+
+#Let's practice some questions. WAP to input 2 numbers and print their sum.
+x=int(input("Enter a number:"))
+y=int(input("Another number:"))
+print("Sum is:",x+y)
+
+#WAP to input side of a square and print its area.
+l=float(input("Enter the side length of a sq."))
+print("Area is:",l*l) #or it can be written as,"l**2", which means l to the power 2.
