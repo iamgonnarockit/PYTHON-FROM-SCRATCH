da66d85a20398c3f1a0467d3c62fbd8ed2943856
iamgonnarockit <adhikarisadikshya097@gmail.com>
2026-03-04 20:50:10 +0545

    How to take input from the user

---
+++ input.py
@@ -0,0 +1,4 @@
+#Let's learn to take inputs from the user.
+a=input("Enter your name:") #result for this is always string
+b=input(int("Enter your age:")) #to take input values
+print("Hi there! Welcome",a,"Ohh! so you're",b,"years old")
