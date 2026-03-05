bf69a0ecb567d87a3c706d235131ee623663e151
iamgonnarockit <adhikarisadikshya097@gmail.com>
2026-03-05 15:58:12 +0545

    String in Python

---
+++ strings.py
@@ -0,0 +1,20 @@
+#String is a data type that stores a sequence of characters.
+#Basic Operations of string
+#1. Concatenation
+a="Sad"
+b="ikshya"
+print(a+b)
+
+#2. Length of string - finds the number of characters used in string
+print("This word consists total of",len(a+b),"characters")
+
+#Slicing in string- It means accessing parts of a string. This is very important in ML.
+#Note: str[ :4] is same as str[0:4]. str[1: ] is same as str[1: len(str)]
+#Negative Index: Negative indexing in Python is a method for accessing elements in a sequence (like a list, tuple, or string) by counting backward from the end. 
+# The last element is accessed using an index of -1, the second-to-last is -2, and so on.
+str="Technology"
+print(str[1:5])
+
+fruits=["Apple","Blueberry","Orange","Pineapple","Mangoes"]
+print(fruits[-1])
+print(fruits[:-2])
